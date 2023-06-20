01e1491e725c2bc76f2083637e8a7c45d3305cfd
Neo-05 <97765977+Neo-05@users.noreply.github.com>
2023-06-20 16:52:04 +0200

    Add files via upload
    update fin 20/06

--- Prod/js.js
+++ Prod/js.js
@@ -59,7 +59,84 @@ setInterval(() =>
 
 document.title = "Plate or Flat"
 
-alert("Tu vas apprendre la vérité")
+// alert("Tu vas apprendre la vérité")
 
+
+// Panier
+
+let oui = 0;
+let nbrGuide = 0;
+let nbrTee = 0;
+
+let Tee = document.getElementById("tee")
+
+// setInterval(() => 
+// {
+
+//     if (nbrTee = 0)
+//     {
+//         Tee.innerHTML = "fds"
+//     }
+//     else
+//     {
+//         Tee.innerHTML = <td>Teeee-Shirt</td> <td>25</td> <td id="nbrTee">0</td>
+//     }
+
+// }, 1000);
+
+let adda = () => {
+
+
+    addcart(25)
+    nbrTee +=1
+    document.getElementById("nbrTee").innerHTML= nbrTee
+}
+
+let addb = () => {
+
+    addcart(15)
+    nbrGuide +=1
+    document.getElementById("nbrGuide").innerHTML= nbrGuide
+
+}
+
+
+
+let suppa = () => {
+
+    if (nbrTee > 0)
+    {
+        suppcart(25)
+        nbrTee -=1
+        document.getElementById("nbrTee").innerHTML= nbrTee
+    }
+
+}
+
+let suppb = () => {
+
+    if(nbrGuide > 0)
+    {
+        suppcart(15)
+        nbrGuide -=1
+        document.getElementById("nbrGuide").innerHTML= nbrGuide
+    }
+
+}
+
+
+
+
+let addcart = (price) => {
+    oui += price;
+
+    document.getElementById("total").innerHTML= oui
+}
+
+let suppcart = (price) => {
+    oui -= price;
+
+    document.getElementById("total").innerHTML= oui
+}
 
 
